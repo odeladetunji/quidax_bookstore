0df36291e293b27cc2334247221fa7ac32595610
odeladetunji <odeladetunji@gmail.com>
2021-06-11 21:00:11 +0100

    added database schema to data.js

--- database/models/data.js
+++ database/models/data.js
@@ -1,84 +1,371 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../dbconnection');
 
-const users = sequelize.define('users', {
+const quidax_books = sequelize.define('quidax_books', {
     id: {
-        field: 'userid',
+        field: 'id',
         type: DataTypes.BIGINT,
         primaryKey: true,
         autoIncrement: true
     },
-    email: {
-        field: 'email',
-        type: DataTypes.TEXT,
-        allowNull: false,
-        isEmail: true,   
-        validate: {
-            notEmpty:{
-                args: true,
-                msg: "Email Address is Required"
-            },
-            len: {
-                args: [13, 25],
-                msg: "Email address must be between the range of 13 to 25 Characters"
-            }
-        }
+    mimeType: {
+        field: 'mime_type',
+        type: DataTypes.BLOB,
+        allowNull: false
     },
-    password: {
-        field: 'password',
-        type: DataTypes.INTEGER,
+    book_picture: {
+        field: 'book_picture',
+        type: DataTypes.BLOB,
+        allowNull: false
+    },
+    create_date: {
+        field: 'create_date',
+        type: DataTypes.DATE,
         allowNull: false,
-        validate: {
-            notEmpty:{
-                args: true,
-                msg: " Password is Required"
-            },
-            len: {
-                args: [6, 8],
-                msg: "Password must be between the range of 6 to 8 Characters"
-            }
-        }
-    }
+    },
+    created_by: {
+        field: 'created_by',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastactivity_by: {
+        field: 'lastactivity_by',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastactivity_date: {
+        field: 'lastactivity_date',
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    price: {
+        field: 'price',
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
 });
 
-const shapes = sequelize.define('shapes', {
+const quidax_book_tags = sequelize.define('quidax_book_tags', {
     id: {
-        field: 'shapeid',
+        field: 'id',
         type: DataTypes.BIGINT,
         primaryKey: true,
         autoIncrement: true
     },
-    email: {
-        field: 'email',
+    book_id: {
+        field: 'book_id',
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    tags: {
+        field: 'tags',
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_uuid: {
+        field: 'user_uuid',
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+    
+});
+
+const quidax_books_users = sequelize.define('quidax_books_users', {
+    id: {
+        field: 'id',
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    user_uuid: {
+        field: 'user_name',
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_name: {
+        field: 'user_name',
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+    
+});
+
+const quidax_books_ratings = sequelize.define('quidax_books_ratings', {
+    id: {
+        field: 'id',
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    ratings: {
+        field: 'ratings',
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    book_id: {
+        field: 'book_id',
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    create_date: {
+        field: 'create_date',
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    user_uuid: {
+        field: 'user_uuid',
         type: DataTypes.STRING,
         allowNull: false,
-        isEmail: true, 
-        validate: {
-            notEmpty:{
-                args: true,
-                msg: "Email Address is Required"
-            },
-            len: {
-                args: [13, 25],
-                msg: "Email address must be between the range of 13 to 25 Characters"
-            }
-        }
     },
-    shapetype: {
-        field: 'shapetype',
-        type: DataTypes.STRING,
+    user_picture: {
+        field: 'user_picture',
+        type: DataTypes.BLOB,
+        allowNull: false,
+    },
+    lastactivity_date: {
+        field: 'lastactivity_date',
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    mimeType: {
+        field: 'mime_type',
+        type: DataTypes.BLOB,
         allowNull: false
     },
-    payload: {
-        field: 'payload',
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    area: {
-        field: 'area',
-        type: DataTypes.INTEGER,
-    }
 });
+
+
+const quidax_books_ratings = sequelize.define('quidax_books_ratings', {
+    id: {
+        field: 'id',
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    quantity: {
+        field: 'quantity',
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    price_per_piece: {
+        field: 'price_per_piece',
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    book_id: {
+        field: 'book_id',
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    create_date: {
+        field: 'create_date',
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    book_title: {
+        field: 'book_title',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    deleted: {
+        field: 'deleted',
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+    user_uuid: {
+        field: 'user_uuid',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+});
+
+const quidax_book_likes = sequelize.define('quidax_book_likes', {
+    id: {
+        field: 'id',
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    book_id: {
+        field: 'book_id',
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    user_uuid: {
+        field: 'user_name',
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    likes: {
+        field: 'likes',
+        type: DataTypes.INTEGER,
+        allowNull: true
+    }
+    
+});
+
+const quidax_book_details = sequelize.define('quidax_book_details', {
+    id: {
+        field: 'id',
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    author: {
+        field: 'author',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    book_id: {
+        field: 'book_id',
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    copies_sold: {
+        field: 'copies_sold',
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    created_by: {
+        field: 'created_by',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    create_date: {
+        field: 'create_date',
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    book_title: {
+        field: 'book_title',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    genre: {
+        field: 'genre',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    publisher: {
+        field: 'publisher',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    title: {
+        field: 'title',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    summary: {
+        field: 'summary',
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    release_date: {
+        field: 'release_date',
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    deleted: {
+        field: 'deleted',
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+    updated_by: {
+        field: 'updated_by',
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+
+});
+
+module.exports = {quidax_books, 
+                  quidax_book_tags, 
+                  quidax_books_ratings, 
+                  quidax_books_users,
+                  quidax_books_shopping_cart,
+                  quidax_book_details,
+                  quidax_book_likes
+                }
+// const users = sequelize.define('users', {
+//     id: {
+//         field: 'userid',
+//         type: DataTypes.BIGINT,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     email: {
+//         field: 'email',
+//         type: DataTypes.TEXT,
+//         allowNull: false,
+//         isEmail: true,   
+//         validate: {
+//             notEmpty:{
+//                 args: true,
+//                 msg: "Email Address is Required"
+//             },
+//             len: {
+//                 args: [13, 25],
+//                 msg: "Email address must be between the range of 13 to 25 Characters"
+//             }
+//         }
+//     },
+//     password: {
+//         field: 'password',
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         validate: {
+//             notEmpty:{
+//                 args: true,
+//                 msg: " Password is Required"
+//             },
+//             len: {
+//                 args: [6, 8],
+//                 msg: "Password must be between the range of 6 to 8 Characters"
+//             }
+//         }
+//     }
+// });
+
+// const shapes = sequelize.define('shapes', {
+//     id: {
+//         field: 'shapeid',
+//         type: DataTypes.BIGINT,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     email: {
+//         field: 'email',
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         isEmail: true, 
+//         validate: {
+//             notEmpty:{
+//                 args: true,
+//                 msg: "Email Address is Required"
+//             },
+//             len: {
+//                 args: [13, 25],
+//                 msg: "Email address must be between the range of 13 to 25 Characters"
+//             }
+//         }
+//     },
+//     shapetype: {
+//         field: 'shapetype',
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     payload: {
+//         field: 'payload',
+//         type: DataTypes.STRING,
+//         allowNull: false
+//     },
+//     area: {
+//         field: 'area',
+//         type: DataTypes.INTEGER,
+//     }
+// });
 
 // async function testDb(){
 // 	try {
@@ -91,4 +378,4 @@ const shapes = sequelize.define('shapes', {
 
 // testDb();
 
-module.exports = {shapes, users}
+
---
+++ routes/create_a_book.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express.Router();
+const modelData = require('../database/model/data');
+const validation = require('../validation/emailAndPassword');
+const tokenValidation = require('../validation/validateToken');
+
+app.get("/", tokenValidation.validateToken, (request, response) => {
+
+    validation.emailValidation(request, response);
+    validation.passwordValidation(request, response);
+
+    modelData.users.findAll({
+        where: {
+              email: request.query.email
+        },
+        limit: 20,
+        OffSet: 0
+    }).then(result => {
+        return response.send(result);
+    })
+});
+
+module.exports = app;
